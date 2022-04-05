2cb89a5965d281c1e15a528f7ed6bc25a3aa65e1
wenlong <y1423027101@gmail.com>
2022-04-06 03:35:03 +0800

    update

--- src/app.js
+++ src/app.js
@@ -36,34 +36,37 @@ let IP = {
                 $$.getElementById(elID).innerHTML = `${resp.data.country_name} ${resp.data.city} ${resp.data.org}`;
             })
     },
-    getWebrtcIP: function() {
-        window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection 
-            || window.webkitRTCPeerConnection;
-        var webrtc = new RTCPeerConnection({ iceServers: []}), i = function() {};
-        webrtc.createDataChannel(""),
-        webrtc.createOffer(webrtc.setLocalDescription.bind(webrtc), i),
-        webrtc.onicecandidate = function(con) {
-            try {
-                if (con && con.candidate && con.candidate.candidate) {
-                    var webctrip = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-                        .exec(con.candidate.candidate)[1];
-                    $$.getElementById("ip-webrtc").innerHTML = webctrip,
-                    webrtc.onicecandidate = i;
-                    $$.getElementById("ip-webrtc-geo").innerHTML = "WebRTC Leaked IP"
-                } else {
-                    $$.getElementById("ip-webrtc").innerHTML = "N/A";
-                }
-            } catch {
-                $$.getElementById("ip-webrtc").innerHTML = "N/A";
-            }
-        }
+    getMyipIP: function() {
+        IP.get(`https://api4.my-ip.io/ip.json?z=${random}`, 'json')
+            .then(resp => {
+                $$.getElementById('ip-myip').innerHTML = resp.data.ip;
+            })
+        IP.get(`https://api6.my-ip.io/ip.json?z=${random}`, 'json')
+            .then(resp => {
+                $$.getElementById('ip-myip-geo').innerHTML = resp.data.ip;
+            })
     },
-    getIpipnetIP: () => {
-        IP.get(`https://myip.ipip.net/?z=${random}`, 'text')
-            .then((resp) => {
-                let data = resp.data.replace('当前 IP：', '').split(' 来自于：');
-                $$.getElementById('ip-ipipnet').innerHTML = `<p id="ip-ipipnet">${data[0]}</p><p class="sk-text-small" id="ip-ipipnet-geo">${data[1]}</p>`;
-            });
+
+    getSpeedtestcnIP: () => {
+        IP.get(`https://forge.speedtest.cn/api/location/info?z=${random}`, 'json')
+            .then(resp => {
+                $$.getElementById('ip-speedtestcn').innerHTML = resp.data.full_ip;
+                $$.getElementById('ip-speedtestcn-geo').innerHTML = `${resp.data.country} ${resp.data.city} ${resp.data.isp}`;
+            })
+    },
+    getIpinfoIP: () => {
+        IP.get(`https://ipinfo.io/json?z=${random}`, 'json')
+            .then(resp => {
+                $$.getElementById('ip-ipinfo').innerHTML = resp.data.ip;
+                $$.getElementById('ip-ipinfo-geo').innerHTML = `${resp.data.country} ${resp.data.city} ${resp.data.org}`;
+            })
+    },
+    getIpgsIP: () => {
+        IP.get(`https://ip.gs/json?z=${random}`, 'json')
+            .then(resp => {
+                $$.getElementById('ip-ipgs').innerHTML = resp.data.ip;
+                $$.getElementById('ip-ipgs-geo').innerHTML = `${resp.data.country} ${resp.data.city} ${resp.data.asn_org}`;
+            })
     },
     getIpipnetIPSimple: () => {
         IP.get(`https://myip.ipip.net/?z=${random}`, 'text')
@@ -72,39 +75,53 @@ let IP = {
             $$.getElementById('ip-ipipnet').innerHTML = `${data[0]} ${data[1]}`;
         });
     },
-    getSohuIP: () => {
-        var script = document.createElement('script');
-        script.src = 'https://pv.sohu.com/cityjson?ie=utf-8'
-        script.onload = () => {
-            if (typeof returnCitySN === 'undefined') {
-                console.log('Failed to load resource: pv.sohu.com')
-            } else {
-                $$.getElementById('ip-sohu').innerHTML = returnCitySN.cip;
-                IP.parseIPMoeip(returnCitySN.cip, 'ip-sohu-geo');
-            }
-        }
-        document.head.appendChild(script);
+    getCfnsIP: () => {
+
+        IP.get(`https://cf-ns.com/cdn-cgi/trace?z=${random}`, 'text')
+        .then((resp) => {
+            prefix = 'ip='
+            titleStart = resp.data.indexOf(prefix)
+            titleEnd = resp.data.indexOf('ts=')
+            ip = resp.data.substr(titleStart+prefix.length, titleEnd-titleStart-prefix.length)
+            loc = resp.data.substr(titleStart+prefix.length)
+            $$.getElementById('ip-cfns').innerHTML = `${ip}`;
+        });
+
     },
-    getIpsbIP: () => {
-        IP.get(`https://api.ip.sb/geoip?z=${random}`, 'json')
+    get138IP: () => {
+
+        IP.get(`https://2022.ip138.com?z=${random}`, 'text')
+        .then((resp) => {
+            prefix = '<title>您的IP地址是：'
+            titleStart = resp.data.indexOf(prefix)
+            titleEnd = resp.data.indexOf('</title>')
+            ip = resp.data.substr(titleStart+prefix.length, titleEnd-titleStart-prefix.length)
+            loc = resp.data.substr(titleStart+prefix.length)
+            locationPrefix = '来自：'
+            locationStart = loc.indexOf('来自：')
+            locationEnd = loc.indexOf('</p>')
+            loc = loc.substr(locationStart+locationPrefix.length, locationEnd-locationStart-locationPrefix.length)
+            $$.getElementById('ip-138').innerHTML = `${ip}`;
+            $$.getElementById('ip-138-geo').innerHTML = `${loc}`;
+        });
+
+    },
+    getIpsbv4IP: () => {
+        IP.get(`https://api-ipv4.ip.sb/geoip?z=${random}`, 'json')
             .then(resp => {
-                $$.getElementById('ip-ipsb').innerHTML = resp.data.ip;
-                $$.getElementById('ip-ipsb-geo').innerHTML = `${resp.data.country} ${resp.data.city} ${resp.data.organization}`;
+                $$.getElementById('ip-ipsbv4').innerHTML = resp.data.ip;
+                $$.getElementById('ip-ipsbv4-geo').innerHTML = `${resp.data.country} ${resp.data.city} ${resp.data.organization}`;
             })
     },
-    getIpifyIP: () => {
-        IP.get(`https://api.ipify.org/?format=json&z=${random}`, 'json')
+
+    getIpsbv6IP: () => {
+        IP.get(`https://api-ipv6.ip.sb/geoip?z=${random}`, 'json')
             .then(resp => {
-                $$.getElementById('ip-ipify').innerHTML = resp.data.ip;
-                return resp.data.ip;
-            })
-            .then(ip => {
-                IP.parseIPIpapi(ip, 'ip-ipify-geo');
-            })
-            .catch(e => {
-                console.log('Failed to load resource: api.ipify.org')
+                $$.getElementById('ip-ipsbv6').innerHTML = resp.data.ip;
+                $$.getElementById('ip-ipsbv6-geo').innerHTML = `${resp.data.country} ${resp.data.city} ${resp.data.organization}`;
             })
     },
+
     getIpapiIP: () => {
         IP.get(`https://ipapi.co/json?z=${random}`, 'json')
             .then(resp => {
